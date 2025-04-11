c6eab2f9f446a469f31b90cc0b98fcaa55676f2e
rowan-board <rowan.board@outlook.com>
2025-04-11 16:25:51 +0100

    commit from home

--- js/main.js
+++ js/main.js
@@ -23,19 +23,24 @@ let globals = {
             [6,6], [7,6], [6,7], [7,7],
             [8,8], [9,8], [8,9], [9,9],
             [10,10], [11,10], [10,11], [11,11]],
-    target_feature: [0, 0, 2, 4, 6, 8, 10, 11]
-
+    target_feature: [0, 0, 2, 4, 6, 8, 10, 11],
+    feedback_delay: 1000
 };
 
 let state = {
+    subject_nr: null,
+    t_start_experiment: null,
+    t_start_trial: null,
+    t_response: null,
     current_dimension: null,
     stage: null,
     trial_number: null,
+    target_feature: null,
     shape_1: null,
     shape_2: null,
     line_1: null,
     line_2: null,
-    choice: null,
+    choice: [],
     reward: null,
     S: null,
     feedback: null,
@@ -45,44 +50,68 @@ let state = {
     stages_passed: null
 };
 
+// object containing trial history so we can update stimuli and blocks according to rules
 let memory = {
     S: [],
+    reward: []
 }
 
-function initalise_task(){
-    state.current_dimension = flip()
-    state.stage = 1
-    state.trial_number = 1
-    set_stimuli()
+function ready(){
+    state.subject_nr = get_subject_nr();
+    console.log('Ready!');
+    $('#gorilla').children().hide();
+    state.t_start_experiment = Date.now();
+    $('#gorilla, #start').show();
+    $('#start-btn').on('click', initialise_task);
+}
+
+function initialise_task(){
+    $('#start').hide();
+    state.current_dimension = flip();
+    state.stage = 0;
+    state.trial_number = 0;
+    start_trial();
+}
+
+function start_trial(){
+    $('#end-block').hide();
+    state.t_start_trial = Date.now();
+    // if 0 then shapes
+    if (state.current_dimension == 0){
+        state.target_feature = globals.shapes[globals.target_feature[state.stage]];
+    } else {
+        state.target_feature = globals.lines[globals.target_feature[state.stage]];
+    }
+    set_stimuli();
 }
 
 function set_stimuli(){
 
-    let s = null;
+    let s = [];
     let n = 0;
     let stim1 = 0;
 
     if (state.stage == 0){
-        s = [globals.stimuli[0], globals.stimuli[1]]
+        s = [globals.stimuli[0], globals.stimuli[1]];
     } else {
         if (state.stage == 1){
-            n = 7
-            stim1 = 2
+            n = 7;
+            stim1 = 2;
         } else if (state.stage == 2){
-            n = 15
-            stim1 = 6
+            n = 15;
+            stim1 = 6;
         } else if (state.stage == 3){
-            n = 23
-            stim1 = 10
+            n = 23;
+            stim1 = 10;
         } else if (state.stage == 4){
-            n = 31
-            stim1 = 14
+            n = 31;
+            stim1 = 14;
         } else if (state.stage == 5){
-            n = 39
-            stim1 = 18
+            n = 39;
+            stim1 = 18;
         } else if (state.stage in [6,7]){
-            n = 47
-            stim1 = 22
+            n = 47;
+            stim1 = 22;
         }
 
         // get flat list of stimuli from previous 3 and 6 trials
@@ -105,22 +134,23 @@ function set_stimuli(){
                 });
 
                 s[0] = stimremain[Math.floor(Math.random() * stimremain.length)];
-                s[1] = globals.stimuli[globals.stimuli.length - 1 - globals.stimuli.indexOf(s[0])];
+                s[1] = globals.stimuli[n - globals.stimuli.indexOf(s[0])];
                 break;
             }
         }
 
         if (rep == 0){
             let tmp_stim = globals.stimuli.slice(stim1, stim1+4);
-            console.log(tmp_stim[Math.floor(Math.random() * tmp_stim.length)]);
+            console.log('s0: ',tmp_stim[Math.floor(Math.random() * tmp_stim.length)]);
+            console.log('s1: ',globals.stimuli[n - globals.stimuli.indexOf(s[0])]);
             s[0] = tmp_stim[Math.floor(Math.random() * tmp_stim.length)];
             s[1] = globals.stimuli[n - globals.stimuli.indexOf(s[0])];
         }
     }
 
     // save stimuli
-    memory.S.push(s)
-    state.S = s
+    memory.S.push(s);
+    state.S = s;
     set_button();
 }
 
@@ -130,28 +160,100 @@ function set_button() {
     if (state.stage == 0){
         if (state.current_dimension == 0){
             // shape state
-            state.shape_1 = globals.shapes[state.S[0]]
-            state.shape_2 = globals.shapes[state.S[1]]
+            state.shape_1 = globals.shapes[state.S[0]];
+            state.shape_2 = globals.shapes[state.S[1]];
             // set stimulus in button dynamically 
             $('#dynamicButton').css('background-image',`url('${state.shape_1}')`);
             $('#dynamicButton_2').css('background-image',`url('${state.shape_2}')`);
         } else {
             // line state
-            state.line_1 = globals.lines[state.S[0]]
-            state.line_2 = globals.lines[state.S[1]]
+            state.line_1 = globals.lines[state.S[0]];
+            state.line_2 = globals.lines[state.S[1]];
             $('#dynamicButton').css('background-image',`url('${state.line_1}')`);
             $('#dynamicButton_2').css('background-image',`url('${state.line_2}')`);
         }
     } else {
         // now do this for all states after simple discrimination, we have compound stimuli now
-        state.shape_1 = globals.shapes[state.S[0][0]]
-        state.shape_2 = globals.shapes[state.S[1][0]]
-        state.line_1 = globals.lines[state.S[0][1]]
-        state.line_2 = globals.lines[state.S[1][1]]    
+        state.shape_1 = globals.shapes[state.S[0][0]];
+        state.shape_2 = globals.shapes[state.S[1][0]];
+        state.line_1 = globals.lines[state.S[0][1]];
+        state.line_2 = globals.lines[state.S[1][1]];   
         $('#dynamicButton').css('background-image',`url('${state.shape_1}'), url('${state.line_1}')`);
         $('#dynamicButton_2').css('background-image',`url('${state.shape_2}'), url('${state.line_2}')`);
     }
+
+    // Randomize which button goes on the left
+    if (Math.random() < 0.5) {
+        $('#stimuli').append($('#dynamicButton'));
+        $('#stimuli').append($('#dynamicButton_2'));
+    } else {
+        $('#stimuli').append($('#dynamicButton_2'));
+        $('#stimuli').append($('#dynamicButton'));
+    }
+
+    // now let's show
+    $('#stimuli').show();
+    $('#stimuli').one('click', get_response);
 }
 
+function get_response(e){
+    $('#stimuli').off('click');
+    let t = state.t_response = Date.now();
+    // Which button was clicked?
+    let btn = $(e.target)[0].id;
+    if (btn == 'dynamicButton'){
+        state.choice = [state.shape_1, state.line_1];
+    } else {
+        state.choice = [state.shape_2, state.line_2];
+    }
+
+    console.log(state.choice);
+    // was it correct
+    if (state.choice.includes(state.target_feature)){
+        state.reward = 1;
+    } else {
+        state.reward = 0;
+    }
+    // append to outcome to memory
+    memory.reward.push(state.reward);
+    setTimeout(show_feedback, globals.feedback_delay);
+}
+
+function show_feedback(){
+    // first hide stimuli then show feedback
+    $('#stimuli').hide()
+    if (state.reward == 1){
+        $('#good-feedback').show();
+    } else {
+        $('#bad-feedback').show();
+    }
+    setTimeout(log_data, globals.feedback_delay);
+}
+
+function log_data(){
+    $('#good-feedback').hide();
+    $('#bad-feedback').hide();
+    primate.metric(state);
+    state.trial_number +=1;
+    let r = memory.reward.slice(-8).flat();
+    let sum = r.reduce((acc, currentValue) => acc + currentValue, 0);
+    // check if last 8 in a row are correct
+    if (sum == 8){
+        state.stage += 1;
+        block_reset();
+    } else {
+        start_trial();
+    }
+}
+
+function block_reset(){
+    // reset reward memory at end of block
+    memory.reward = [];
+    $('#end-block').show();
+    setTimeout(start_trial, globals.feedback_delay);
+}
+
+
+
 // Call the function when the page loads
-window.onload = initalise_task;
+window.onload = ready;
